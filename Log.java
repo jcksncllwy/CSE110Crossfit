f225299a5ce982095566f545c9406444534bb9e4
Kilroy Higgins <KilroyHiggins@gmail.com>
2011-05-22 15:51:31 -0700

    Added licensing commentary.

--- Log.java
+++ Log.java
@@ -5,6 +5,26 @@
  * This class is designed to create a Log object, which holds all info
  * entered by the user stored in the WOD, Biometric, and Notes classes.
  */
+
+/*    
+*	This file is part of Crossfit++.
+*
+*    Crossfit++ is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    Crossfit++ is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+
 package crossfitPlusPlus;
 
 import java.util.*;
--- WOD.java
+++ WOD.java
@@ -5,6 +5,25 @@
  * This class is designed to create a Workout of the Day (WOD) object, 
  * which holds all of the info for a WOD.
  */
+
+/*    
+*	This file is part of Crossfit++.
+*
+*    Crossfit++ is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    Crossfit++ is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
 package crossfitPlusPlus;
 
 import java.util.*;
